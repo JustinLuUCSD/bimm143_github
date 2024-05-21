3f5288fd94f27c31e324368e59d745fad017edbb
Justin Lu <julu@ucsd.edu>
2024-05-21 11:54:39 -0700

    add class 5

---
+++ Class04/Class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Lab Class 4: R Basics"
+#' author: "Justin Lu (PID: A16318305)"
+#' date: "April 11, 2024"
+#' ---
+
+#This is just a text file. 
+
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ = "l", col = "purple", lwd = 3)
